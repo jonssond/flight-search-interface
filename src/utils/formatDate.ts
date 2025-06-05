03fec9ff27b6fb3d570f2561631975ccfd89d02d
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-05 14:39:46 -0300

    fix: update formatDate function to convert Date to string in DD/MM/YYYY HH:mm format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,9 @@
-export function formatISODateToLocalDate(date: Date) {
-  const [actualDate, time] = date.toString().split('T');
-  const [year, month, day] = actualDate.split('-');
-  const [hours, minutes] = time.split(':');
+export function formatDateToStringDate(date: Date): string {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // getMonth() is 0-indexed
+  const year = date.getFullYear();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
   return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
