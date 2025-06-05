c9c02740a1101776d73f33e8fb687792ed2c8909
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-05 15:42:46 -0300

    feat: refactor Header component to manage favorites with props

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,35 @@
 import './header.css';
 import { ReactComponent as StarIcon } from '../../assets/icons/star-svgrepo-com.svg';
 
-export const Header = () => {
+interface HeaderProps {
+  onFavoritesClick: () => void;
+  showingFavorites: boolean;
+}
+
+export const Header = ({ onFavoritesClick, showingFavorites }: HeaderProps) => {
   return (
     <header className="header-container">
       <div className="header-logo">
         <p>SearchFlight</p>
       </div>
       <div className="header-favorites">
-        <span className="star-icon">
+        <button
+          className={`favorites-button ${showingFavorites ? 'active' : ''}`}
+          onClick={onFavoritesClick}
+          title={
+            showingFavorites
+              ? 'Mostrar todos os voos'
+              : 'Mostrar apenas favoritos'
+          }
+        >
           <StarIcon
-            fill="gold"
+            fill={showingFavorites ? 'gold' : 'none'}
             width="48"
             height="48"
             stroke="gold"
             strokeWidth={1}
           />
-        </span>
+        </button>
       </div>
     </header>
   );
