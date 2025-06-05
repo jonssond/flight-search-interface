4abf8991c248c976045bda7eded1d79ca306cdea
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-05 15:14:30 -0300

    feat: add sorting functionality to table component

--- src/components/table/types.ts
+++ src/components/table/types.ts
@@ -1,4 +1,13 @@
+export type SortOrder = 'asc' | 'desc' | null;
+
+export type SortConfig = {
+  column: string;
+  order: SortOrder;
+};
+
 export type tableProps = {
   columns: string[];
   data: unknown[][];
+  onSort?: (column: string, order: SortOrder) => void;
+  sortConfig?: SortConfig;
 };
