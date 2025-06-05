33552a020b35c0480467e98491d6ef5e5d0b200e
Diogo Jonsson <diogo.jonsson@hotmail.com>
2025-06-05 15:43:07 -0300

    feat: add DateInput component with styled container and label

--- src/components/form/date-input/DateInput.tsx
+++ src/components/form/date-input/DateInput.tsx
@@ -1,11 +1,12 @@
 import { dateInputProps } from './types';
+import './date-input.css';
 
 export const DateInput = (props: dateInputProps) => {
   const { label, value, onChange } = props;
 
   return (
-    <div>
-      <p>{label}</p>
+    <div className="date-input-container">
+      <label>{label}</label>
       <input type="date" value={value} onChange={onChange} />
     </div>
   );
